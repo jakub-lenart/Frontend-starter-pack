b5fe28b298b78e055ec8963acf2702e98e115f91
Jakub Lenart <jakub@lenart.tech>
2017-04-23 15:44:41 +0200

    Update style.sass

--- gulp/style.js
+++ gulp/style.js
@@ -1,31 +1,31 @@
 const
-    gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sassLint = require('gulp-sass-lint');
+	gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	cleanCSS = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sassLint = require('gulp-sass-lint');
 
 gulp.task('css', () =>
-    gulp.src('src/sass/**/*.sass')
-        .pipe(sassLint({
-            files: {
-                ignore: 'src/sass/assets/_reset.sass'
-            },
-            rules: {
-                'property-sort-order': 0,
-                'indentation': 0,
-                'no-ids': 0,
-                'single-line-per-selector': 0
-            }
-        }))
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'))
+	gulp.src('src/sass/**/*.sass')
+		.pipe(sassLint({
+			files: {
+				ignore: 'src/sass/assets/_reset.sass'
+			},
+			rules: {
+				'property-sort-order': 0,
+				'indentation': 0,
+				'no-ids': 0,
+				'single-line-per-selector': 0
+			}
+		}))
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError())
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('dist/css'))
 );
 
 gulp.task('watch:css', ['css'], () =>
-    gulp.watch('src/sass/**/*.sass', ['css'])
+	gulp.watch('src/sass/**/*.sass', ['css'])
 );
